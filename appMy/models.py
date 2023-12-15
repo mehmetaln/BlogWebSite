782d283589ac25d835b065fd0721a283f79e28a6
Ali ALİN <mehmettalnn@gmail.com>
2023-12-15 23:06:08 +0300

    end

--- appMy/models.py
+++ appMy/models.py
@@ -7,8 +7,20 @@ class Blog(models.Model):
     title = models.CharField(("Başlık"), max_length=50)
     text= models.TextField(("Blog Yazısı"))    
     image = models.ImageField(("Resim"), upload_to="blog",)
-    date_now = models.DateTimeField(("Tarih - Saat"), auto_now_add=False)
-    
+    date_now = models.DateTimeField(("Tarih - Saat"), auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="user2", verbose_name=("Begenen Kullanıcılar"),blank=True)
+    commnet_num = models.IntegerField(("Yorum Sayisi"), default=0)
     
     def __str__(self):
         return self.title
+
+
+
+class Comment(models.Model):
+    user= models.ForeignKey(User, verbose_name=("Kullanıcı"), on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, verbose_name=("Yorum Yapılan Blog"), on_delete=models.CASCADE)
+    text = models.TextField(("Yorum"))
+    date_now = models.DateTimeField(("Tarih - Saat"), auto_now_add=True)
+    
+    def __str__(self):
+        return self.blog.title
--- appMy/views.py
+++ appMy/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from appMy.models import Blog
+from appMy.models import *
+from django.db.models import Count
+
 # Create your views here.
 
 
 def indexPage(request):
     blog_list = Blog.objects.all()
     blog_random_list = Blog.objects.all().order_by('?')
+    blog_comments = Comment.objects.all()
+    blog_likes = Blog.objects.annotate(q_count = Count('likes')).order_by("-q_count")
+    # blog_comments = Blog.objects.all().order_by('-comment_num')
+    
     context = {
         "blog_list":blog_list,
-        "blog_random_list": blog_random_list[:4]
+        "blog_random_list": blog_random_list[:4],
+        "blog_comments": blog_comments[:4],
+        "blog_likes":blog_likes[:5]
+        
     }
     return render(request, "index.html", context)
 
@@ -16,11 +25,22 @@ def indexPage(request):
 
 def detailPage(request,bid):
     blog= Blog.objects.get(id=bid)
+    comment_list = Comment.objects.filter(blog=blog)
     
     
+    if request.method == "POST":
+      text = request.POST.get("text")
+      # request.user => girişli kullanıcı
+      comment = Comment(text=text, blog=blog, user=request.user)
+      comment.save()
+      
+      blog.comment_num += 1
+      blog.save()
     
     
     context = {
         "blog":blog,
+        "comment_list":comment_list
     }
+    
     return render(request, "detail.html", context)
