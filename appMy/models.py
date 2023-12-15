fcc2b767cee7ba62797e5b9fefd61c6fac56caa9
Ali ALİN <mehmettalnn@gmail.com>
2023-12-15 16:14:45 +0300

    detailpage yapıldı

--- appMy/models.py
+++ appMy/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, related_name = "user1", verbose_name=("Kullanıcı"), on_delete=models.CASCADE)
+    title = models.CharField(("Başlık"), max_length=50)
+    text= models.TextField(("Blog Yazısı"))    
+    image = models.ImageField(("Resim"), upload_to="blog",)
+    date_now = models.DateTimeField(("Tarih - Saat"), auto_now_add=False)
+    
+    
+    def __str__(self):
+        return self.title
--- appMy/views.py
+++ appMy/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-
+from appMy.models import Blog
 # Create your views here.
 
 
 def indexPage(request):
+    blog_list = Blog.objects.all()
+    blog_random_list = Blog.objects.all().order_by('?')
     context = {
-        
+        "blog_list":blog_list,
+        "blog_random_list": blog_random_list[:4]
     }
     return render(request, "index.html", context)
+
+
+
+def detailPage(request,bid):
+    blog= Blog.objects.get(id=bid)
+    
+    
+    
+    
+    context = {
+        "blog":blog,
+    }
+    return render(request, "detail.html", context)
