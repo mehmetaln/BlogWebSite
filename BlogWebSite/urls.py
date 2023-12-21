db8f909687d11326bf0c06e4cabec23c0c5c1af2
Ali ALİN <mehmettalnn@gmail.com>
2023-12-21 18:23:29 +0300

    a

--- BlogWebSite/urls.py
+++ BlogWebSite/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     path('all/<cslug>',allblogPage, name="allblogPage2"),
     # User
     path('login/',loginPage, name="loginPage"),
-    path('logout/',logoutPage, name="logoutPage"),
+    path('logout/user/',logoutPage, name="logoutPage"),
     
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
